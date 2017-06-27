b5072e8b4d2bce4f4bf1545abb91d4e0208384f0
afa1989@126.com <zhangqufa123>
2017-06-27 23:24:45 +0800

    order ui

--- app/src/main/java/com/ssj/hulijie/pro/firstpage/view/OrderActivity.java
+++ app/src/main/java/com/ssj/hulijie/pro/firstpage/view/OrderActivity.java
@@ -2,7 +2,9 @@ package com.ssj.hulijie.pro.firstpage.view;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.ssj.hulijie.R;
 import com.ssj.hulijie.mvp.presenter.impl.MvpBasePresenter;
@@ -10,11 +12,17 @@ import com.ssj.hulijie.pro.base.view.BaseActivity;
 import com.ssj.hulijie.utils.TitlebarUtil;
 
 
+
 /**
  * Created by Administrator on 2017/5/15.
  */
 
 public class OrderActivity extends BaseActivity implements View.OnClickListener {
+    private TextView order_buy_count;  //购买数量
+    private int count = 1;
+    private Button order_sub;
+
+
     @Override
     public MvpBasePresenter bindPresenter() {
         return null;
@@ -29,7 +37,11 @@ public class OrderActivity extends BaseActivity implements View.OnClickListener 
 
     private void initView() {
         initToolbar();
-
+        order_sub = (Button)findViewById(R.id.order_sub);
+        findViewById(R.id.order_add).setOnClickListener(this);
+        order_sub.setOnClickListener(this);
+        order_sub.setClickable(false);
+        order_buy_count = (TextView)findViewById(R.id.order_buy_count);
     }
 
     private void initToolbar() {
@@ -44,5 +56,26 @@ public class OrderActivity extends BaseActivity implements View.OnClickListener 
 
     @Override
     public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.order_add:
+                count++;
+                changeBuyCount();
+                break;
+            case R.id.order_sub:
+                if (count > 1) {
+                    count--;
+                    changeBuyCount();
+                }
+                break;
+        }
+    }
+
+    private void changeBuyCount(){
+        if (count > 1) {
+           order_sub.setClickable(true);
+        } else if (count == 1) {
+            order_sub.setClickable(false);
+        }
+        order_buy_count.setText(count + "");
     }
 }
